ce9e12087863cb7ba6ede0368fbf7f9a477f17f6
bibek-in-github <mahatcoding@gmail.com>
2023-07-14 08:31:23 -0700

    feat:add searchBox

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -10,14 +10,39 @@ class Home extends StatelessWidget {
       backgroundColor: tdBGColor,
       appBar: _buildAppBar(),
       body: Container(
+        padding: const EdgeInsetsDirectional.symmetric(horizontal: 20.0, vertical: 15.0),
         child: Column(
           children: [
-            
+            searchBox(),
           ],
         ),
       ),
     );
   }
+}
+
+Widget searchBox() {
+  return Container(
+    padding: const EdgeInsetsDirectional.symmetric(horizontal: 15.0),
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(20.0),
+    ),
+    child: TextField(
+      decoration: InputDecoration(
+          contentPadding: const EdgeInsets.all(0.0),
+          prefixIcon: Icon(
+            Icons.search,
+            color: Colors.black,
+            size: 20.0,
+          ),
+          prefixIconConstraints:
+              BoxConstraints(maxHeight: 20.0, minWidth: 25.0),
+          border: InputBorder.none,
+          hintText: 'Search',
+          hintStyle: TextStyle(color: tdGrey)),
+    ),
+  );
 }
 
 AppBar _buildAppBar() {
